1d7b2ca52dbff00b577dcda5527dec991ddccdcb
Kushagra Saxena <faaltuwebsitekeliye@gmail.com>
2022-12-27 15:28:15 +0530

    second commit

---
+++ .history/pages/index_20221227152646.js
@@ -0,0 +1,10 @@
+
+
+const Home = () => {
+  return (
+    <div>Hello</div>
+    <img
+  )
+}
+
+export default Home
--- .history/pages/index_20221227152723.js
+++ .history/pages/index_20221227152723.js
@@ -3,7 +3,7 @@ const Home = () => {
     <div>
       <div className=" hover:scale-110 text-white text-5xl font-bold rounded-lg w-[17.5rem] h-[1rem]">
         <img
-          src="/images/images/photo.jpg"
+          src="/image/image"
           alt="no"
         />
       </div>
